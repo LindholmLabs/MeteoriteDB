45fe50b67031bf34bfe8bb20ad83fdf5ce72c3fb
William Lindholm <william_lindholm@outlook.com>
2023-05-03 15:04:27 +0200

    Created Meteorite activity for detailed view of meteorite.

---
+++ app/src/main/java/com/example/project_assignment/meteorite_activity.java
@@ -0,0 +1,14 @@
+package com.example.project_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class meteorite_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meteorite);
+    }
+}
