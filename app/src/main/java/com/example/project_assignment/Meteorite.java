7bfda111ec6fcc0e15929ec9bb54f71bcaaf7eca
William Lindholm <william_lindholm@outlook.com>
2023-05-04 16:58:28 +0200

    Implemented Parceable interface.

--- app/src/main/java/com/example/project_assignment/Meteorite.java
+++ app/src/main/java/com/example/project_assignment/Meteorite.java
@@ -1,9 +1,14 @@
 package com.example.project_assignment;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 
-public class Meteorite {
+public class Meteorite implements Parcelable {
     protected String name;
     protected String id;
     protected String mass;
@@ -17,6 +22,7 @@ public class Meteorite {
     @SerializedName(value = "reclong", alternate = "longitude")
     protected String longitude;
 
+
     @Override
     public String toString() {
         return "Meteorite{" +
@@ -27,5 +33,42 @@ public class Meteorite {
                 ", latitude='" + latitude + '\'' +
                 ", longitude='" + longitude + '\'' +
                 '}';
+    }
+    protected Meteorite(Parcel in) {
+        name = in.readString();
+        id = in.readString();
+        mass = in.readString();
+        date = in.readString();
+        latitude = in.readString();
+        longitude = in.readString();
+    }
+
+    public static final Creator<Meteorite> CREATOR = new Creator<Meteorite>() {
+        @Override
+        public Meteorite createFromParcel(Parcel in) {
+            return new Meteorite(in);
+        }
+
+        @Override
+        public Meteorite[] newArray(int size) {
+            return new Meteorite[size];
+        }
+    };
+
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(id);
+        parcel.writeString(mass);
+        parcel.writeString(date);
+        parcel.writeString(latitude);
+        parcel.writeString(longitude);
     }
 }
