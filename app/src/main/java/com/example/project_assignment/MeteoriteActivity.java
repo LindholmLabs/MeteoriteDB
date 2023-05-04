e1d11a034300e31c66dbd5624d7d3d6605b37f1a
William Lindholm <william_lindholm@outlook.com>
2023-05-04 16:58:50 +0200

    Now fetching intent onCreate.

--- app/src/main/java/com/example/project_assignment/MeteoriteActivity.java
+++ app/src/main/java/com/example/project_assignment/MeteoriteActivity.java
@@ -2,13 +2,19 @@ package com.example.project_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class meteorite_activity extends AppCompatActivity {
+public class MeteoriteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meteorite);
+
+        Intent intent = getIntent();
+        Meteorite meteorite = (Meteorite) intent.getParcelableExtra("meteorite");
+
     }
 }
